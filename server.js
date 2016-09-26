7a64c2752e82e451ad04697a314b8aac40e2879d
ChristianSotos <csotos@hawk.iit.edu>
2016-09-26 10:10:08 -0700

    initial setup/file structure

--- server.js
+++ server.js
@@ -1,20 +1,45 @@
-// Packages
+// Require the Express Module
 var express = require('express');
+//Require the Mongoose Module
+var mongoose = require('mongoose');
+// Require body-parser (to receive post data from clients)
 var bodyParser = require('body-parser');
+// Require path
 var path = require('path');
-// App init
+//For loading model files
+var fs = require('fs')
+//For session data
+var session = require('express-session')
+var sessionConfig = {
+	secret: 'CookieMonster',
+	resave: true,
+	saveUninitialized: true,
+	name: 'myCookie',
+	cookie: {
+		secure: false,
+		httpOnly: false,
+		maxAge: 3600000
+	}
+}
+
+// Create an Express App
 var app = express();
-// DB Config
-require('./server/config/mongoose.js');
-// Middle-Ware
+
+// Integrate body-parser with our App
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static(path.join(__dirname,'/client/static')));
-// App routes
+// Setting our Static Folder Directory
+app.use(express.static(path.join(__dirname, './client')));
+//Use session
+app.use(session(sessionConfig))
+
+//Load models
+require('./server/config/mongoose.js')
+
+//Connect to routes
 var routes_setter = require('./server/config/routes.js');
 routes_setter(app);
-
-// Start App
-app.listen(8000, function () {
-  console.log("listening on Port 8000");
+// Setting our Server to Listen on Port: 8000
+app.listen(8000, function() {
+    console.log("listening on port 8000");
 })
--- server/models/User.js
+++ server/models/User.js
@@ -1,9 +1,7 @@
 var mongoose = require('mongoose');
 
 var UserSchema = new mongoose.Schema({
-  firstName: String,
-  lastName: String,
-  birthday: Date
+	
 });
 
 mongoose.model('User', UserSchema);
