ec1ef265853bd6d0ba49f29bb727d7f08d88037e
ChristianSotos <csotos@hawk.iit.edu>
2016-09-28 15:52:34 -0700

    fixed items

--- server/controllers/Users.js
+++ server/controllers/Users.js
@@ -60,15 +60,19 @@ module.exports = {
 		})
 	},
 	items: function(req,res){
-		User.findOne({_id:req.session.current_user._id}).exec(function(err, user){
+		Event.find({_attendees:{$in:[req.session.current_user._id]}}).exec(function(err, user_events){
 			if(err){
 				res.sendStatus(500)
 			}else{
-				Item.find({_event: {$in:user._events}}).populate('_users').exec(function(err, all_items){
+				var event_ids = []
+				for(var i = 0 ; i < user_events.length ; i++){
+					event_ids.push(user_events[i]._id)
+				}
+				Item.find({_event: {$in:event_ids}}).populate('_users').exec(function(err, all_items){
 					if(err){
 						res.sendStatus(500)
 					}else{
-						Item.find({_users: {$in:[user._id]}}).populate('_users').exec(function(err, user_items){
+						Item.find({_users: {$in:[req.session.current_user._id]}}).populate('_users').exec(function(err, user_items){
 							if(err){
 								res.sendStatus(500)
 							}else{
