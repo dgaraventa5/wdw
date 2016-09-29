2063b061d24f52d53b146937ffc8e90c697bd206
ChristianSotos <csotos@hawk.iit.edu>
2016-09-29 12:34:08 -0700

    photo shit

--- client/angular/controllers/eventscreen_controller.js
+++ client/angular/controllers/eventscreen_controller.js
@@ -1,6 +1,9 @@
 app.controller('eventscreenController', ['$scope', '$location', '$routeParams', 'eventFactory', 'userFactory', 'itemFactory', function($scope, $location, $routeParams, eventFactory, userFactory, itemFactory){
 	eventFactory.getEvent($routeParams.eid, function(event){
 		$scope.event = event
+		$scope.style = {
+			'background-image' : 'url('+$scope.event.image+')'
+		}
 	})
 	userFactory.getUsers(function(users){
 		$scope.users = users
